c5134d2140aaf6646d736988233fd32409197903
Geovana Macena <macenageovana9@gmail.com>
2025-08-06 17:22:23 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+
+    int main() {
+    
+    int x;
+    float y;
+    float s;
+    x=30;
+    y=5.5;
+    s=x/y;
+    
+    printf("A divisão dos valores x e y eh: %f",s);
+}
